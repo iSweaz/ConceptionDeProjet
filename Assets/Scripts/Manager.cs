ff27b8d13166c86e82dfdcfce0c4c3c83cc270ec
Romain Legrand <legrandromain1602@gmail.com>
2025-11-28 13:08:17 +0100

    Select card

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Manager : MonoBehaviour
@@ -8,9 +10,11 @@ public class Manager : MonoBehaviour
 
     public float timeRemaining = 5;
 
-    private int compteurItem = 0;
+    [SerializeField]private int compteurItem = 0;
     private int lengthDeck = 0;
 
+    private string Answer; 
+    private Selection player; // À changer quand on commencera le multi. Voir si on fait une préfab ou si chaque joueur à sa propre scène et à ce moment pas besoin d'y touché
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -25,28 +29,44 @@ public class Manager : MonoBehaviour
         timer = FindFirstObjectByType<Timer>();
         timer.timeRemaining = timeRemaining;
         timer.timerIsRunning = true;
+
+        player = GetComponent<Selection>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if( compteurItem < deck.US.Length)
+        //Probablement à moove sur le singleton pour la com serveur, par encore réfléchit à ça
+        if( compteurItem + 1 < deck.US.Length)
         {
             if(timer.timeRemaining == 0)
             {
-                Debug.Log("Change Item");
                 compteurItem++;
                 timer.timeRemaining = timeRemaining;
                 timer.timerIsRunning = true;
 
-                blackBoard.ChangeDisplay(compteurItem);
+                blackBoard.ChangeDisplay(compteurItem); // à retirer, présent pour check le fonctionnement du changement de US
+                Answer = "";
+                player.answer = "i";
+                player.ChangeColor(null);
             }
         }
-        else
+        else if(timer.timeRemaining == 0)
         {
-            Destroy(timer);
-            Time.timeScale = 0;
+            Answer = player.answer;
+            Debug.Log(Answer);
+            timer.timeText.text = "";
+            Time.timeScale = 0;   
         }
     }
 
+    // void loadCard()
+    // {
+    //     GameObject Parent =  GameObject.Find("Cards");
+    //     GameObject[] Cards =  new GameObject[11];
+    //     for (int i = 0; i < Cards.Length; i++)
+    //     {
+    //         Cards[i] = Parent.transform.GetChild(i).gameObject;
+    //     }
+    // }
 }
---
+++ Assets/Scripts/Selection.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class Selection : MonoBehaviour
+{
+    Camera cam;
+    public string answer ="i";
+
+    private GameObject lastObject;
+    private Renderer m_ObjectRenderer;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+
+    }
+
+    void  Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            RayCast();
+        }
+    }
+
+    void RayCast()
+    {
+        RaycastHit hit;
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit,15) )
+        { 
+            Debug.Log(hit.transform.CompareTag("Card"));
+            if(hit.transform.CompareTag("Card"))
+            {
+                answer = hit.transform.name;
+                ChangeColor(hit.transform.gameObject);
+            }
+        }
+    }
+    public void ChangeColor(GameObject gameObject)
+    {
+        if(lastObject)
+        {
+            m_ObjectRenderer.materials[1].color = Color.red;
+        } 
+        if(gameObject)
+        {
+            m_ObjectRenderer = gameObject.GetComponent<Renderer>();
+            //Change the GameObject's Material Color to red
+            m_ObjectRenderer.materials[1].color = Color.green;
+            lastObject = gameObject;   
+        }        
+    }
+}
