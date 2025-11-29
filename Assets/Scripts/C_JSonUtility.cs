50ece0f78a42ed027451761c5b0f8b6c0f9a6fcf
Panta <yeleen.bernard@yahoo.com>
2025-11-29 13:02:00 +0100

    Fix JSON
    Nouveau fonctionnement pour la gestion des fichiers JSON (sans interface définie)

---
+++ Assets/Scripts/C_JSonUtility.cs
@@ -0,0 +1,38 @@
+using Newtonsoft.Json;
+using System.IO;
+using UnityEngine;
+
+/// <summary>
+/// Classe qui contient les fonctions de gestion des fichiers JSON
+/// </summary>
+public static class C_JSonUtility
+{
+    /// <summary>
+    /// Fonction de sauvegarde des fichiers JSON
+    /// </summary>
+    /// <param name="usList">Liste qui contient l'ensemble des users stories</param>
+    /// <param name="path">Chemin de sauvegarde du fichier JSON</param>
+    public static void SaveDeck(USJsonFile deck, string path)
+    {
+        string json = JsonConvert.SerializeObject(deck, Newtonsoft.Json.Formatting.Indented);
+        File.WriteAllText(path, json);
+    }
+
+    /// <summary>
+    /// Fonction de chargement des fichiers JSON
+    /// </summary>
+    /// <param name="path">Chemin de chargement du fichier JSON</param>
+    /// <returns>Deck chargé ou null en cas de problème</returns>
+    public static USJsonFile LoadDeck(string path)
+    {
+        if (!File.Exists(path))
+        {
+            Debug.LogError("Fichier JSON introuvable : " + path);
+            return null;
+        }
+
+        string json = File.ReadAllText(path);
+        USJsonFile deck = JsonConvert.DeserializeObject<USJsonFile>(json);
+        return deck;
+    }
+}
